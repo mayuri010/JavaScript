8565f02839988d5c6d2075417f36eeaa51c78c6f
Mayuri Kodavala <meghalkodavala010@gmail.com>
2026-01-28 16:19:50 +0530

    [MK][Java script data type]

---
+++ datatypes.js
@@ -0,0 +1,145 @@
+//DataTypes
+//1.Primitive 2.Non primitive
+
+let num1=1;
+let num2=2;
+console.log("number", num1,num2);
+
+
+//String
+
+let name="Mayuri";
+let name2="Misa";
+console.log("names:",name,name2);
+
+//Boolean
+let isTrue=true;
+let isFalse=false;
+console.log("boolean:",isTrue);
+
+//Undefined
+
+let a;
+console.log("undefined",a);
+
+//BigInt
+let BigInt=12345678910123;
+console.log(BigInt);
+
+//2.Non-primitive
+
+//Object
+
+let Student={
+    name:"Ma",
+    roll:"65",
+    address:{
+        city:"ksd",
+    }
+}
+console.log(Student);
+
+//Array
+
+let c = [1,2,3,4,5,6]
+console.log(c);
+
+
+
+//Function
+
+// function greet(){
+//     console.log("This isFunction");
+// }
+// greet();
+
+// function showValues(value){
+//     console.log("This is Value",value);
+//     if(value==20){
+//         console.log("number ----");
+//     } 
+// }
+// showValues();
+// showValues(20);
+// showValues([1,2,3]);
+
+// //Addition
+
+// const add=(num1=5,num2=5)=>{
+//     return num1+num2
+// }
+// console.log('this is arrow function.:',add())
+
+// // Substraction
+
+// const sub=(num1=10,num2=5)=>{
+//     return num1-num2
+// }
+// console.log('this is arrow fun.:',sub);
+
+// //Multiplication
+
+// const mul=(num1=5,num2=10)=>{
+//     return num1*num2
+// }
+// console.log('this is arrow function.:',mul());
+
+//Division
+
+// const div=(num1=20,num2=10)=>{
+//     return num1/num2
+// }
+// console.log('this is arrow function.:',div());
+
+
+
+
+// // Assignment operator
+// let value=10;
+// console.log('DEFAULT VALUE:',value);
+
+// value +=5 //value + 5
+// console.log(value);
+
+// value -=5 //value - 5
+// console.log(value);
+
+// value *=5 //value * 5
+// console.log(value);
+
+// value /=5 //value / 5
+// console.log(value);
+
+
+//logical operator
+
+// let age=20;
+// let hasId=false
+// let name="student"
+// let obj={
+//     name:'ex',
+//     rollno:245
+// }
+
+// console.log("Logical AND (&&):", age>18 && hasId);
+// console.log("Logical OR (||):", age<18 || hasId);
+// console.log("Logical NOT (!):",  ! hasId);
+
+// console.log(typeof age)
+
+//Logical Operator
+
+// let value = 10;
+// let num="10"
+
+// console.log("value == num:",value == num);
+// console.log("value === num:",value === num);
+// console.log("value != num:",value!=num);
+
+
+
+
+
+
+
+
