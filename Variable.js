604e4ec2d5b5583f349cc453e8e67efc4e096b55
Mayuri Kodavala <meghalkodavala010@gmail.com>
2026-01-28 17:52:32 +0530

    [MK] [Variables program]

---
+++ Variable.js
@@ -0,0 +1,20 @@
+//Var
+// var a = 10;
+// console.log(a);
+
+//let
+//  let a=20;
+//  if(true){
+//     let a=10;
+//  }
+//  console.log(a);
+
+//const
+
+const pi =3.14;
+console.log(pi);
+
+
+
+
+ 
